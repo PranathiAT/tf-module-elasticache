62b6bdb1867ef6d42f8994b73e5547466ad824a2
Pranathi Alapati <tpranathi04@gmail.com>
2023-06-02 18:06:16 +0530

    elasticache

--- vars.tf
+++ vars.tf
@@ -11,8 +11,6 @@ variable "kms_arn" {}
 variable "port_no" {
   default = 6379
 }
-variable "instance_count" {}
-variable "instance_class" {}
 variable "node_type" {}
 variable "num_node_groups" {}
 variable "replicas_per_node_group" {}
